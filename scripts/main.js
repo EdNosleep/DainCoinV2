817cbddc02adf15fc12fe6f67525358cef02d876
EdNosleep <zapasnoy.ed.veis@gmail.com>
2025-11-12 18:15:21 +0300

    main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@
 // ===============================
 
 // Импорт базовых модулей
-//import { initInspector } from './scripts/inspector.js';
-//import { startCoin } from './scripts/coin.js';
+// import { initInspector } from './scripts/inspector.js';
+// import { startCoin } from './scripts/coin.js';
 // (позже добавим)
 // import { startCoin } from './coin.js';
 // import { initEffects } from './coineffect.js';
@@ -34,8 +34,8 @@ loadingText.style.marginBottom = '8vh';
 gameContainer.appendChild(loadingText);
 
 // ========== ПОДКЛЮЧЕНИЕ МОДУЛЕЙ ==========
-//startCoin();
-//initInspector();
+// startCoin();
+// initInspector();
 // initEffects(gameContainer);
 
 // ========== АДАПТАЦИЯ ПОД ТЕЛЕФОНЫ ==========
